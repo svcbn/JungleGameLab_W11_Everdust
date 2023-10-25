0efbe38111d338232e7ee8f490681115309baef0
haein Oh <badarangdev@gmail.com>
2023-10-25 12:53:52 +0900

    Feat: Make Spear And Spear Animation

---
+++ Assets/ProPlatformer/_Scripts/Haein/MouseLook.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
