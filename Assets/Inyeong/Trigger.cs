1cabdc43a4a6aa973d74e2bbc5788e3c9cd672f3
Park In Yeong <50985650+clwmrndl92@users.noreply.github.com>
2023-10-27 16:10:23 +0900

    Add Trigger Platform Set
    prefab LTSet : Light Trigger Set

---
+++ Assets/Inyeong/Trigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    protected virtual void  Awake() {}
+    protected virtual void  Start() {}
+    protected virtual void  Update() {}
+    public virtual void OnTrigger() {}
+}
