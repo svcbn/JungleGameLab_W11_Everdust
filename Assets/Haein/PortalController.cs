cb8aaf0fa4d6b05d1658ec8aa762cb0bf1f456ca
haein Oh <badarangdev@gmail.com>
2023-10-27 16:57:08 +0900

    포탈 제작 완

---
+++ Assets/Haein/PortalController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalController : MonoBehaviour
+{
+    public GameObject textParent;
+    private bool isPlayerIn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (isPlayerIn)
+            {
+                //스테이지 변경
+                //SceneManager.LoadScene("Stage2");
+                Debug.Log("스테이지 변경됨");
+            }
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textParent.SetActive(true);
+            isPlayerIn = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textParent.SetActive(false);
+            isPlayerIn = false;
+        }
+    }
+}
